2f9c311e9bb89e47129f467298d5d2ba89ad76c4
NikolayIz <nikolay97altov@gmail.com>
2025-05-16 22:18:02 +0300

    add test with fixtures

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -12,11 +12,25 @@ class UtilsTest extends TestCase
     // Метод (функция), определенный внутри класса,
     // Должен начинаться со слова test
     // Ключевое слово public нужно, чтобы PHPUnit мог вызвать этот тест снаружи
+    public function getFixtureFullPath($fixtureName)
+    {
+        $parts = [__DIR__, 'fixtures', $fixtureName];
+        return realpath(implode('/', $parts));
+    }
+    
     public function testReverse(): void
     {
         // Сначала идет ожидаемое значение (expected)
         // И только потом актуальное (actual)
         $this->assertEquals('', reverseString(''));
         $this->assertEquals('olleh', reverseString('hello'));
+
+        $pathToOriginal = $this->getFixtureFullPath('original.txt');
+        $pathToExpected = $this->getFixtureFullPath('expected.txt');
+
+        $original = file_get_contents($pathToOriginal);
+        $originalReverse = reverseString($original);
+
+        $this->assertStringEqualsFile($pathToExpected, $originalReverse);
     }
 }
